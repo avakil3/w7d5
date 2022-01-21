7e52f0c9d9b49e6cbb0bc54ab34e49b693dfdc70
avakil3 <aagamvakil@hotmail.com>
2022-01-21 12:04:30 -0500

    finished application controller

--- reddit_clone/app/controllers/application_controller.rb
+++ reddit_clone/app/controllers/application_controller.rb
@@ -1,5 +1,31 @@
 class ApplicationController < ActionController::Base
 
+	protect_from_forgery with: :exception
+
+	helper_method :current_user, :logged_in?
+
+	def current_user
+		@current_user ||= User.find_by(session_token: session[:session_token])
+	end
+
+	def ensure_logged_in
+		# if current_user
+		# else #no current_user
+		# 	redirect_to new_session_url
+		# end
+		redirect_to new_session_url if current_user.nil?
+	end
+
+	def login(user)
+		@current_user = user
+		session[:session_token] = user.reset_session_token!
+	end
+
+	def logged_in?
+		!!current_user
+		# !current_user.nil?
+	end
+
     def logout
         current_user.reset_session_token!
         session[:session_token] = nil
