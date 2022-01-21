f701c3c8859529964a84d6ea7f5bd2ff2a18560a
Ka Yi Leung <email@email.com87kleung@gmail.comgit config user.email>
2022-01-21 11:34:49 -0800

    finished posts controller

--- reddit_clone/app/controllers/posts_controller.rb
+++ reddit_clone/app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+	before_action :ensure_logged_in
+	before_action :is_user_the_author?, only:[:edit, :update]
 
 	def new
 		@post = Post.new
@@ -10,4 +12,48 @@ class PostsController < ApplicationController
 		render :show
 	end
 
+	def create
+		@post = Post.new(post_params)
+		@post.sub_id = params[:sub_id]
+		@post.user_id = current_user.id
+
+		if @post.save
+			redirect_to post_url(@post)
+		else
+			flash.now[:errors] = @post.errors.full_messages
+			render :new
+		end
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+
+		if @post && is_user_the_author?	
+		else
+			flash.now[:error] = ["User is not the author of the post"]
+		end
+
+		render :edit
+	end
+
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(post_params)
+			redirect_to post_url(@post)
+		else
+			flash.now[:errors] = @post.errors.full_messages
+			render :edit
+		end
+	end
+
+	def is_user_the_author?
+		@post.user_id == current_user.id
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :url, :content)
+	end
 end
