718f2bca54e207e9878d19a2071e7ce460b69e1c
Ka Yi Leung <email@email.com87kleung@gmail.comgit config user.email>
2022-01-21 14:13:53 -0800

    view

--- reddit_clone/app/models/sub.rb
+++ reddit_clone/app/models/sub.rb
@@ -2,12 +2,12 @@ class Sub < ApplicationRecord
     validates :title, presence: true
 
     belongs_to :moderator,
-        primary_id: :id,
+        # primary_id: :id,
         foreign_key: :user_id,
         class_name: :User
 
 	has_many :posts,
-		primary_id: :id,
+		# primary_id: :id,
 		foreign_key: :sub_id,
 		class_name: :Post
 
--- reddit_clone/config/routes.rb
+++ reddit_clone/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :users do
-    resources :subs, only:[:create, :edit, :update]
+    resources :subs, only:[:create, :edit, :update, :new]
   end
   resource :session
-  resources :subs, except:[:create, :edit, :update]
-  resources :posts, execpt:[:index]
+  resources :subs, except:[:create, :edit, :update, :new]
+  resources :posts, except:[:index]
 end 
 
 
